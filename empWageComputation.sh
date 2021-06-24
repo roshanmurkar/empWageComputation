2352dca08841e2e45618a4990ca5fc2bb0719486
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-24 20:14:37 +0530

    storing all data

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -234,3 +234,47 @@ totalSalary="$( getEmpWage $totalEmpHrs )"
 
 echo "--------------------------------------------------------"
 
+
+#uc9-storing all data
+
+
+
+isPartTime=1;
+isFullTime=2;
+maxHrsInMonth=100;
+empRatePerHr=20;
+numWorkingDays=20;
+
+totalEmpHrs=0;
+totalWorkingDays=0;
+function getWorkHrs() {
+        case $1 in
+                $isFullTime)
+                        empHrs=8
+                        ;;
+                $isPartTime)
+                        empHrs=4
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+        echo $empHrs
+}
+
+function getEmpWage() {
+        echo $(($1*$empRatePerHr))
+}
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3));
+        empHrs="$(getWorkHrs $empCheck)"
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+        dailyWages[$totalWorkingDays]=$(($empHrs*$empRatePerHr))
+done
+
+totalSalary="$( getEmpWage $totalEmpHrs )"
+
+echo " End of program"
