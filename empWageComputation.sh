52b126e38686a0627b1c38fdafd02204784e8aa1
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-24 20:00:40 +0530

    store wage values

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -191,3 +191,45 @@ done
 totalSalaryfun=$(($totalWorkingHours*$empRatePerHr));
 
 echo "--------------------------------------------------------"
+
+#uc8-store wage value
+
+isPartTime=1;
+isFullTime=2;
+maxHrsInMonth=100;
+empRatePerHr=20;
+numWorkingDays=20;
+
+totalEmpHrs=0;
+totalWorkingDays=0;
+function getWorkHrs() {
+        case $1 in
+                $isFullTime)
+                        empHrs=8
+                        ;;
+                $isPartTime)
+                        empHrs=4
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+        echo $empHrs
+}
+
+function getEmpWage() {
+        echo $(($1*$empRatePerHr))
+}
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3));
+        empHrs="$(getWorkHrs $empCheck)"
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+        dailyWages[$totalWorkingDays]=$(($empHrs*$empRatePerHr))
+done
+
+totalSalary="$( getEmpWage $totalEmpHrs )"
+
+echo "--------------------------------------------------------"
