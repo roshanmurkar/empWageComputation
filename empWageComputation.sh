5542156aebe5e6de2267a45763bb91bc2189884c
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-24 10:58:35 +0530

    Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
-#uc1
+#uc0
 echo "Welcome to Employee Wage Computation Program"
 
-#uc2
+#uc1
 isPresent=1;
 randomCheck=$((RANDOM%2));
 
@@ -13,4 +13,33 @@ then
 else
         echo "Employee is absent"
 fi
+
+
+#uc2-calculate daily empWage
+
+emp1=1;
+emp2=2;
+emp3=3;
+wagePerHr=20;
+#hrInDay=8;
+#totalWageInDay=0;
+empCheck=$((RANDOM%4))
+
+case $empCheck in
+        $emp1)
+                empHrs=8
+                ;;
+        $emp2)
+                empHrs=4
+                ;;
+	$emp3)
+		empHrs=2
+		;;
+        *)
+                empHrs=0
+                ;;
+esac
+totalWageInDay=$(($empHrs*$wagePerHr))
+
+
 
