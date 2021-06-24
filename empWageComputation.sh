384d165fc61c619aeaf8b361d0a119e41b014aa7
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-24 11:05:39 +0530

    add PartTime

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -14,3 +14,47 @@ else
         echo "Employee is absent"
 fi
 
+#uc2-calculate daily empWage
+
+emp1=1;
+emp2=2;
+emp3=3;
+wagePerHr=20;
+empCheck=$((RANDOM%4))
+
+case $empCheck in
+        $emp1)
+                empHrs=8
+                ;;
+        $emp2)
+                empHrs=4
+                ;;
+        $emp3)
+                empHrs=2
+                ;;
+        *)
+                empHrs=0
+                ;;
+esac
+totalWageInDay=$(($empHrs*$wagePerHr))
+
+#uc3-add Part Time
+
+#uc2-calculate daily empWage
+
+isPartTime=1;
+empRatePerHr=20;
+empCheck=$((RANDOM%2))
+
+case $empCheck in
+        $isPartTime)
+                empHrs=8
+                ;;
+        *)
+                empHrs=0
+                ;;
+esac
+salarypartTime=$(($empHrs*$empRatePerHr))
+
+
+
