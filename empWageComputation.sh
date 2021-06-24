c124ca6efcb8d4f1ce970a5d55eea71b0bb412e8
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-24 10:29:56 +0530

    Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 
+#uc1
 echo "Welcome to Employee Wage Computation Program"
+
+#uc2
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ]
+then
+        echo "Employee is present"
+else
+        echo "Employee is absent"
+fi
+
